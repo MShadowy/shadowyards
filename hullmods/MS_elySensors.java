af9163a30e6bec99ca677525afe6e5fcfead9b7e
MShadowy <mshadowy.Art@gmail.com>
2017-05-21 07:07:59 -0600

    Add files via upload

--- hullmods/MS_elySensors.java
+++ hullmods/MS_elySensors.java
@@ -4,30 +4,22 @@ import com.fs.starfarer.api.combat.BaseHullMod;
 import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MS_elySensors extends BaseHullMod {
     
     private static final float RANGEFINDER = 25f;
-    private static final Map mag = new HashMap();
-    static {
-        mag.put(HullSize.FRIGATE, 360f);
-        mag.put(HullSize.DESTROYER, 180f);
-        mag.put(HullSize.CRUISER, 90f);
-        mag.put(HullSize.CAPITAL_SHIP, 90f);
-    }
+    private static final float SENSOR_BOOST = 50F;
     
     @Override
     public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
         stats.getSightRadiusMod().modifyPercent(id, RANGEFINDER);
-        stats.getSensorStrength().modifyPercent(id, (Float) mag.get(hullSize));
+        stats.getSensorStrength().modifyPercent(id, SENSOR_BOOST);
     }
     
     @Override
     public String getDescriptionParam(int index, HullSize hullSize) {
 	if (index == 0) return "" + (int) RANGEFINDER;
-        if (index == 1) return "" + ((Float) mag.get(hullSize)).intValue();
+        if (index == 1) return "" + (int) SENSOR_BOOST;
 	return null;
     }
     
