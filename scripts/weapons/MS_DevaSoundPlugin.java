42ebc74bdbe734710fe85056b3aa5687e979e7eb
MShadowy <mshadowy.Art@gmail.com>
2017-07-12 15:34:38 -0600

    Add files via upload

--- scripts/weapons/MS_DevaSoundPlugin.java
+++ scripts/weapons/MS_DevaSoundPlugin.java
@@ -5,7 +5,9 @@ import com.fs.starfarer.api.combat.CombatEngineAPI;
 import com.fs.starfarer.api.combat.EveryFrameWeaponEffectPlugin;
 import com.fs.starfarer.api.combat.WeaponAPI;
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.List;
+import org.lazywizard.lazylib.FastTrig;
 import org.lazywizard.lazylib.MathUtils;
 import org.lazywizard.lazylib.VectorUtils;
 import org.lwjgl.util.vector.Vector2f;
@@ -17,7 +19,7 @@ public class MS_DevaSoundPlugin implements EveryFrameWeaponEffectPlugin {
     private static final float CHARGEUP_PARTICLE_DISTANCE_MAX = 60.0f;
     private static final float CHARGEUP_PARTICLE_SIZE_MIN = 1.0f;
     private static final float CHARGEUP_PARTICLE_SIZE_MAX = 4.0f;
-    private static final float CHARGEUP_PARTICLE_ANGLE_SPREAD = 60.0f;
+    private static final float CHARGEUP_PARTICLE_ANGLE_SPREAD = 180.0f;
     private static final float CHARGEUP_PARTICLE_DURATION = 0.2f;
     private static final float CHARGEUP_PARTICLE_COUNT_FACTOR = 5.0f;
     private static final float CHARGEUP_PARTICLE_BRIGHTNESS = 0.7f;
@@ -25,10 +27,11 @@ public class MS_DevaSoundPlugin implements EveryFrameWeaponEffectPlugin {
     private static final Vector2f ZERO = new Vector2f();
     
     public static final float MAX_OFFSET = 10f; 
-    public static final float SWEEP_INTERVAL = 0.14f;
+    public static final float SWEEP_INTERVAL = 0.95f;
     
     protected float timer = 0;
     protected int dir = 1;
+    private List <Float> ANGLES = new ArrayList();
     
     private float last_charge_level = 0.0f;
     private boolean charging = false;
@@ -38,16 +41,16 @@ public class MS_DevaSoundPlugin implements EveryFrameWeaponEffectPlugin {
     
     @Override
     public void advance(float amount, CombatEngineAPI engine, WeaponAPI weapon) {
-        if (Global.getCombatEngine().isPaused()) {
+        if (engine.isPaused()) {
             return;
         }
         
         Vector2f point1 = new Vector2f(weapon.getLocation());
         Vector2f offset1 = new Vector2f();
         if (weapon.getSlot().isTurret()) {
-            offset1 = new Vector2f(40f, 0f);
+            offset1 = new Vector2f(13.5f, 9f);
         } else if (weapon.getSlot().isHardpoint()) {
-            offset1 = new Vector2f(51f, 0f);
+            offset1 = new Vector2f(26f, 9f);
         }
         VectorUtils.rotate(offset1, weapon.getCurrAngle(), offset1);
         Vector2f.add(offset1, point1,point1);
@@ -55,50 +58,41 @@ public class MS_DevaSoundPlugin implements EveryFrameWeaponEffectPlugin {
         Vector2f point2 = new Vector2f(weapon.getLocation());
         Vector2f offset2 = new Vector2f();
         if (weapon.getSlot().isTurret()) {
-            offset2 = new Vector2f(40f, 5f);
+            offset2 = new Vector2f(13.5f, -9f);
         } else if (weapon.getSlot().isHardpoint()) {
-            offset2 = new Vector2f(51f, 5f);
+            offset2 = new Vector2f(26f, -9f);
         }
         VectorUtils.rotate(offset2, weapon.getCurrAngle(), offset2);
         Vector2f.add(offset2, point2,point2);
-        
-        Vector2f point3 = new Vector2f(weapon.getLocation());
-        Vector2f offset3 = new Vector2f();
-        if (weapon.getSlot().isTurret()) {
-            offset3 = new Vector2f(40f, -5f);
-        } else if (weapon.getSlot().isHardpoint()) {
-            offset3 = new Vector2f(51f, -5f);
-        }
-        VectorUtils.rotate(offset3, weapon.getCurrAngle(), offset3);
-        Vector2f.add(offset3, point3,point3);
         
         float charge_level = weapon.getChargeLevel();
         
         if (!runOnce) {
             runOnce = true;
+            ANGLES=new ArrayList<>(weapon.getSpec().getTurretAngleOffsets());
+            
             weapon.ensureClonedSpec();
         }
-        
-        /*float side = 0;
-        
-        for(int i=0; i<weapon.getSpec().getTurretFireOffsets().size(); i++){
-        if (weapon.getSpec().getTurretFireOffsets().get(i).x > 0) {
-        side = 1;
-        } else {
-        side = -1;
-        }
-        }*/
-        
-        if (restart) {
-            int pick = MathUtils.getRandomNumberInRange(1, 0);
-            float side = (pick - 0.5f);
-            float angle = 20f * side;
                     
+        if (restart) {
             for(int i=0; i<weapon.getSpec().getTurretAngleOffsets().size(); i++){
-                weapon.getSpec().getHardpointAngleOffsets().set(i, angle);
-                weapon.getSpec().getTurretAngleOffsets().set(i, angle);
-                weapon.getSpec().getHiddenAngleOffsets().set(i, angle);
+                weapon.getSpec().getHardpointAngleOffsets().set(i, ANGLES.get(i));
+                weapon.getSpec().getTurretAngleOffsets().set(i, ANGLES.get(i));
+                weapon.getSpec().getHiddenAngleOffsets().set(i, ANGLES.get(i));
             }
+        }
+        
+        if(weapon.getChargeLevel()>=0.99f){
+            restart=false;
+            timer+=amount*SWEEP_INTERVAL;
+            for(int i=0; i<weapon.getSpec().getTurretAngleOffsets().size(); i++){            
+                weapon.getSpec().getHardpointAngleOffsets().set(i, (-1+2*i)*MAX_OFFSET*-1*(float)FastTrig.cos(timer));            
+                weapon.getSpec().getTurretAngleOffsets().set(i, (-1+2*i)*MAX_OFFSET*-1*(float)FastTrig.cos(timer));            
+                weapon.getSpec().getHiddenAngleOffsets().set(i, (-1+2*i)*MAX_OFFSET*-1*(float)FastTrig.cos(timer));  
+            }
+        }else if(!restart){
+            timer=0;
+            restart=true;
         }
         
         if (charging) {
@@ -130,8 +124,6 @@ public class MS_DevaSoundPlugin implements EveryFrameWeaponEffectPlugin {
                             point1, distance, (angle + weapon.getCurrAngle()));
                         Vector2f spawn_location2 = MathUtils.getPointOnCircumference(
                             point2, distance, (angle + weapon.getCurrAngle() + 45f));
-                        Vector2f spawn_location3 = MathUtils.getPointOnCircumference(
-                            point3, distance, (angle + weapon.getCurrAngle() - 45f));
                         // speed from "distance to muzzle" as required by specified duration
                         speed = distance / CHARGEUP_PARTICLE_DURATION;
                         particle_velocity = MathUtils.getPointOnCircumference(
@@ -150,14 +142,6 @@ public class MS_DevaSoundPlugin implements EveryFrameWeaponEffectPlugin {
                         );
                         engine.addHitParticle(
                             spawn_location2,
-                            particle_velocity,
-                            size,
-                            CHARGEUP_PARTICLE_BRIGHTNESS,
-                            CHARGEUP_PARTICLE_DURATION,
-                            CHARGEUP_PARTICLE_COLOR
-                        );
-                        engine.addHitParticle(
-                            spawn_location3,
                             particle_velocity,
                             size,
                             CHARGEUP_PARTICLE_BRIGHTNESS,
@@ -183,39 +167,6 @@ public class MS_DevaSoundPlugin implements EveryFrameWeaponEffectPlugin {
                                                                                                                                              * 0.3f, 0.2f,
                                                         new Color(MathUtils.getRandomNumberInRange(200, 255), MathUtils.getRandomNumberInRange(150, 200),
                                                                   MathUtils.getRandomNumberInRange(50, 150), 255));
-                    Global.getCombatEngine().addHitParticle(point3, ZERO, (float) Math.random() * 8f + 8f * weapon.getChargeLevel(), weapon.getChargeLevel()
-                                                                                                                                             * 0.3f, 0.2f,
-                                                        new Color(MathUtils.getRandomNumberInRange(200, 255), MathUtils.getRandomNumberInRange(150, 200),
-                                                                  MathUtils.getRandomNumberInRange(50, 150), 255));
-                    timer += amount;
-                    
-                    List<Float> offsets = null;
-                    if (weapon.getSlot().isHardpoint())
-                        offsets = weapon.getSpec().getHardpointAngleOffsets();
-                    else if (weapon.getSlot().isTurret())
-                        offsets = weapon.getSpec().getTurretAngleOffsets();
-                    else if (weapon.getSlot().isHidden());
-                        offsets = weapon.getSpec().getHiddenAngleOffsets();
-                    if (offsets == null) return;
-                    
-                    float currOffset = offsets.get(0);
-                    float newOffset = currOffset + amount/SWEEP_INTERVAL * dir;
-                    if (newOffset > MAX_OFFSET) {
-                        newOffset = MAX_OFFSET;
-                        dir *= -1;
-                    } else if (newOffset < -MAX_OFFSET) {
-                        newOffset = -MAX_OFFSET;
-                        dir *= -1;
-                    }
-                    
-                    offsets.remove(0);
-                    offsets.add(0, newOffset);
-                    
-                    for(int i=0; i<weapon.getSpec().getTurretAngleOffsets().size(); i++){
-                        weapon.getSpec().getHardpointAngleOffsets().set(i, (Float) newOffset);
-                        weapon.getSpec().getTurretAngleOffsets().set(i, newOffset);
-                        weapon.getSpec().getHiddenAngleOffsets().set(i, newOffset);
-                    }
                 }
             } 
         } else {
@@ -223,10 +174,6 @@ public class MS_DevaSoundPlugin implements EveryFrameWeaponEffectPlugin {
                 charging = true;
                 Global.getSoundPlayer().playSound("ms_slowBeamH_charge", 1, 1, point1, weapon.getShip().getVelocity());
             }   
-        }
-        
-        if (!weapon.isFiring()) {
-            restart = true;
         }
         
         last_charge_level = charge_level;
--- scripts/weapons/MS_MurtiSoundPlugin.java
+++ scripts/weapons/MS_MurtiSoundPlugin.java
@@ -5,7 +5,9 @@ import com.fs.starfarer.api.combat.CombatEngineAPI;
 import com.fs.starfarer.api.combat.EveryFrameWeaponEffectPlugin;
 import com.fs.starfarer.api.combat.WeaponAPI;
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.List;
+import org.lazywizard.lazylib.FastTrig;
 import org.lazywizard.lazylib.MathUtils;
 import org.lazywizard.lazylib.VectorUtils;
 import org.lwjgl.util.vector.Vector2f;
@@ -14,61 +16,84 @@ public class MS_MurtiSoundPlugin implements EveryFrameWeaponEffectPlugin {
     
     private static final Color CHARGEUP_PARTICLE_COLOR = new Color(130, 190, 160, 100);
     private static final float CHARGEUP_PARTICLE_DISTANCE_MIN = 4.0f;
-    private static final float CHARGEUP_PARTICLE_DISTANCE_MAX = 40.0f;
+    private static final float CHARGEUP_PARTICLE_DISTANCE_MAX = 60.0f;
     private static final float CHARGEUP_PARTICLE_SIZE_MIN = 1.0f;
     private static final float CHARGEUP_PARTICLE_SIZE_MAX = 4.0f;
     private static final float CHARGEUP_PARTICLE_ANGLE_SPREAD = 180.0f;
-    private static final float CHARGEUP_PARTICLE_DURATION = 0.15f;
-    private static final float CHARGEUP_PARTICLE_COUNT_FACTOR = 7.0f;
+    private static final float CHARGEUP_PARTICLE_DURATION = 0.2f;
+    private static final float CHARGEUP_PARTICLE_COUNT_FACTOR = 5.0f;
     private static final float CHARGEUP_PARTICLE_BRIGHTNESS = 0.7f;
     
     private static final Vector2f ZERO = new Vector2f();
     
     public static final float MAX_OFFSET = 10f; 
-    public static final float SWEEP_INTERVAL = 0.14f;
+    public static final float SWEEP_INTERVAL = 0.95f;
+    private List <Float> ANGLES = new ArrayList();
     
+    protected float currOffset = 0;
     protected float timer = 0;
     protected int dir = 1;
-        
+    
     private float last_charge_level = 0.0f;
     private boolean charging = false;
     private boolean firing = false;
-    private boolean restart = false;
+    private boolean restart = true;
     private boolean runOnce = false;
     
     @Override
     public void advance(float amount, CombatEngineAPI engine, WeaponAPI weapon) {
-        if (Global.getCombatEngine().isPaused()) {
+        if (engine.isPaused()) {
             return;
         }
         
-        Vector2f point = new Vector2f(weapon.getLocation());
-        Vector2f offset = new Vector2f();
+        Vector2f point1 = new Vector2f(weapon.getLocation());
+        Vector2f offset1 = new Vector2f();
         if (weapon.getSlot().isTurret()) {
-            offset = new Vector2f(23f, 5f);
+            offset1 = new Vector2f(17f, 9f);
         } else if (weapon.getSlot().isHardpoint()) {
-            offset = new Vector2f (17f, 5f);
+            offset1 = new Vector2f(20f, 9f);
         }
-        VectorUtils.rotate(offset, weapon.getCurrAngle(), offset);
-        Vector2f.add(offset, point,point);
+        VectorUtils.rotate(offset1, weapon.getCurrAngle(), offset1);
+        Vector2f.add(offset1, point1,point1);
+        
+        Vector2f point2 = new Vector2f(weapon.getLocation());
+        Vector2f offset2 = new Vector2f();
+        if (weapon.getSlot().isTurret()) {
+            offset2 = new Vector2f(17f, -9f);
+        } else if (weapon.getSlot().isHardpoint()) {
+            offset2 = new Vector2f(20f, -9f);
+        }
+        VectorUtils.rotate(offset2, weapon.getCurrAngle(), offset2);
+        Vector2f.add(offset2, point2,point2);
         
         float charge_level = weapon.getChargeLevel();
         
         if (!runOnce) {
             runOnce = true;
+            ANGLES=new ArrayList<>(weapon.getSpec().getTurretAngleOffsets());
+            
             weapon.ensureClonedSpec();
         }
-        
-        if (restart) {
-            int pick = MathUtils.getRandomNumberInRange(1, 0);
-            float side = (pick - 0.5f);
-            float angle = 20f * side;
                     
+        if (restart) {
             for(int i=0; i<weapon.getSpec().getTurretAngleOffsets().size(); i++){
-                weapon.getSpec().getHardpointAngleOffsets().set(i, angle);
-                weapon.getSpec().getTurretAngleOffsets().set(i, angle);
-                weapon.getSpec().getHiddenAngleOffsets().set(i, angle);
+                weapon.getSpec().getHardpointAngleOffsets().set(i, ANGLES.get(i));
+                weapon.getSpec().getTurretAngleOffsets().set(i, ANGLES.get(i));
+                weapon.getSpec().getHiddenAngleOffsets().set(i, ANGLES.get(i));
             }
+        }
+        
+        if(weapon.getChargeLevel()>=0.99f){
+            restart=false;
+            timer+=amount*SWEEP_INTERVAL;
+            for(int i=0; i<weapon.getSpec().getTurretAngleOffsets().size(); i++){            
+                weapon.getSpec().getHardpointAngleOffsets().set(i, (-1+2*i)*MAX_OFFSET*-1*(float)FastTrig.cos(timer));            
+                weapon.getSpec().getTurretAngleOffsets().set(i, (-1+2*i)*MAX_OFFSET*-1*(float)FastTrig.cos(timer));            
+                weapon.getSpec().getHiddenAngleOffsets().set(i, (-1+2*i)*MAX_OFFSET*-1*(float)FastTrig.cos(timer));  
+            }
+        }else if(!restart){
+            timer=0;
+            restart=true;
         }
         
         if (charging) {
@@ -96,8 +121,10 @@ public class MS_MurtiSoundPlugin implements EveryFrameWeaponEffectPlugin {
                             -0.5f * CHARGEUP_PARTICLE_ANGLE_SPREAD,
                             0.5f * CHARGEUP_PARTICLE_ANGLE_SPREAD);
                         // spawn location
-                        Vector2f spawn_location = MathUtils.getPointOnCircumference(
-                            point, distance, (angle + weapon.getCurrAngle()));
+                        Vector2f spawn_location1 = MathUtils.getPointOnCircumference(
+                            point1, distance, (angle + weapon.getCurrAngle()));
+                        Vector2f spawn_location2 = MathUtils.getPointOnCircumference(
+                            point2, distance, (angle + weapon.getCurrAngle() + 45f));
                         // speed from "distance to muzzle" as required by specified duration
                         speed = distance / CHARGEUP_PARTICLE_DURATION;
                         particle_velocity = MathUtils.getPointOnCircumference(
@@ -107,7 +134,15 @@ public class MS_MurtiSoundPlugin implements EveryFrameWeaponEffectPlugin {
                         );
                     
                         engine.addHitParticle(
-                            spawn_location,
+                            spawn_location1,
+                            particle_velocity,
+                            size,
+                            CHARGEUP_PARTICLE_BRIGHTNESS,
+                            CHARGEUP_PARTICLE_DURATION,
+                            CHARGEUP_PARTICLE_COLOR
+                        );
+                        engine.addHitParticle(
+                            spawn_location2,
                             particle_velocity,
                             size,
                             CHARGEUP_PARTICLE_BRIGHTNESS,
@@ -120,53 +155,26 @@ public class MS_MurtiSoundPlugin implements EveryFrameWeaponEffectPlugin {
             } else {
                 firing = true;
                 
+                restart=false;
+                
                 if (weapon.isFiring()) {
-                    Global.getCombatEngine().addHitParticle(point, ZERO, (float) Math.random() * 15f + 15f * weapon.getChargeLevel(), weapon.getChargeLevel()
+                    Global.getCombatEngine().addHitParticle(point1, ZERO, (float) Math.random() * 8f + 8f * weapon.getChargeLevel(), weapon.getChargeLevel()
+                                                                                                                                             * 0.3f, 0.2f,
+                                                        new Color(MathUtils.getRandomNumberInRange(200, 255), MathUtils.getRandomNumberInRange(150, 200),
+                                                                  MathUtils.getRandomNumberInRange(50, 150), 255));
+                    Global.getCombatEngine().addHitParticle(point2, ZERO, (float) Math.random() * 8f + 8f * weapon.getChargeLevel(), weapon.getChargeLevel()
                                                                                                                                              * 0.3f, 0.2f,
                                                         new Color(MathUtils.getRandomNumberInRange(200, 255), MathUtils.getRandomNumberInRange(150, 200),
                                                                   MathUtils.getRandomNumberInRange(50, 150), 255));
                 }
-                
-                List<Float> offsets = null;
-                    if (weapon.getSlot().isHardpoint())
-                        offsets = weapon.getSpec().getHardpointAngleOffsets();
-                    else if (weapon.getSlot().isTurret())
-                        offsets = weapon.getSpec().getTurretAngleOffsets();
-                    else if (weapon.getSlot().isHidden());
-                        offsets = weapon.getSpec().getHiddenAngleOffsets();
-                    if (offsets == null) return;
-                    
-                    float currOffset = offsets.get(0);
-                    float newOffset = currOffset + amount/SWEEP_INTERVAL * dir;
-                    if (newOffset > MAX_OFFSET) {
-                        newOffset = MAX_OFFSET;
-                        dir *= -1;
-                    } else if (newOffset < -MAX_OFFSET) {
-                        newOffset = -MAX_OFFSET;
-                        dir *= -1;
-                    }
-                    
-                    offsets.remove(0);
-                    offsets.add(0, newOffset);
-                    
-                    for(int i=0; i<weapon.getSpec().getTurretAngleOffsets().size(); i++){
-                        weapon.getSpec().getHardpointAngleOffsets().set(i, (Float) newOffset);
-                        weapon.getSpec().getTurretAngleOffsets().set(i, newOffset);
-                        weapon.getSpec().getHiddenAngleOffsets().set(i, newOffset);
-                    }
             } 
         } else {
             if (weapon.getChargeLevel() >0f && weapon.getCooldownRemaining() <= 0f) {
                 charging = true;
-                Global.getSoundPlayer().playSound("ms_slowBeamM_charge", 1, 1, point, weapon.getShip().getVelocity());
+                Global.getSoundPlayer().playSound("ms_slowBeamH_charge", 1, 1, point1, weapon.getShip().getVelocity());
             }   
-        }
-        
-        if (!weapon.isFiring()) {
-            restart = true;
         }
         
         last_charge_level = charge_level;
     }
-    
 }
